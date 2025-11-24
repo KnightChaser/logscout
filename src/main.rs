23400f7f14f885d878f98b3321561d7f35e74f7b
knightchaser <agerio100@naver.com>
2025-11-24 16:57:57 +0900

    Chore: Correct inconsistent logging format

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use std::sync::mpsc;
 
 fn main() {
     if let Err(err) = run() {
-        eprintln!("log-scout: error: {err}");
+        eprintln!("[logscout]: error: {err}");
         std::process::exit(1);
     }
 }
